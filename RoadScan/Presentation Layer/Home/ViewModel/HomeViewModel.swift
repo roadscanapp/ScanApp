e4a39e86c55ff9ac77dfdec423e6783ac56bc6e0
Aybergen <ai_bolat@kbtu.kz>
2023-04-03 17:05:40 +0600

    Markers on map

---
+++ RoadScan/Presentation Layer/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewModel.swift
+//  RoadScan
+//
+//  Created by Aibergen on 30.03.2023.
+//
+
+import Foundation
+
+struct PinViewModel {
+    let latitude: Double
+    let longitude: Double
+    let nameOfLocation: String
+}
