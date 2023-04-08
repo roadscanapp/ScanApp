70f73537fe7d1d4f00d2600e015ef0c73ef125b4
YernarKhassen <aseeayou@mail.ru>
2023-04-09 02:16:41 +0600

    feat: fix review comments

--- RoadScan/Presentation Layer/Home/Controller/HomeViewController+Extenstion.swift
+++ RoadScan/Presentation Layer/Home/Controller/HomeViewController+Extenstion.swift
@@ -1,5 +1,14 @@
 import GoogleMaps
 
+// MARK: - CoreMotionService Delegate
+extension HomeViewController: CoreMotionServiceDelegate {
+    func getCoordinateMotionDevice(with data: CoreMotionViewModel) {
+        print("x ---------->", data.xPosition)
+        print("y ---------->", data.yPosition)
+        print("z----------->", data.zPosition)
+    }
+}
+
 // MARK: - LocationService Delegate
 extension HomeViewController: LocationServiceProtocol {
     func getCurrentLocation(with location: CurrentLocationModel) {
